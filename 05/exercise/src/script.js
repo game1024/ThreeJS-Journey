6b2995ad8e6f3132fbdd02be5dd467b166098aa8
game1024 <168624508+game1024@users.noreply.github.com>
2025-03-31 08:19:20 +0800

    Add animation exercise
    Add animation exercise

---
+++ 05/exercise/src/script.js
@@ -0,0 +1,63 @@
+import * as THREE from "three";
+import gsap from "gsap";
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+camera.position.y = 1;
+camera.lookAt(mesh.position);
+
+// // Clock
+// const clock = new THREE.Clock()
+
+gsap.to(mesh.rotation, {
+  repeat: -1,
+  duration: 4,
+  delay: 0,
+  ease: "linear",
+  y: 2 * Math.PI,
+});
+
+// Animation
+const tick = () => {
+  // // Clock
+  // const elapsedTime = clock.getElapsedTime()
+
+  // // Update objects
+  // camera.position.y = 1.5 * Math.sin(elapsedTime)
+  // camera.position.x = 1.5 * Math.cos(elapsedTime)
+  // camera.lookAt(mesh.position)
+
+  // Render
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
